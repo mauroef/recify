50c35a3b21e7912e73299f131e2a977547e002d1
mauroef <cypress90@gmailcom>
2020-03-07 22:03:49 -0300

    change uri

--- src/config/paths.js
+++ src/config/paths.js
@@ -1,1 +1,2 @@
-export const apiUri = 'http://192.168.1.62:80/recify-backend/';
+// export const apiUri = 'http://192.168.1.62:80/recify-backend/';
+export const apiUri = 'http://mauroef.duckdns.org/';
